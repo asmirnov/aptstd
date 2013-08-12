7c4e2b2e87fddf760c6892233313e1522f3a7e8a
Alexander Smirnov <devops@asmirnov.info>
2013-08-12 10:27:57 +0300

    Bumped version.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "devops@asmirnov.info"
 license          "Apache 2.0"
 description      "Configures apt for standard repositories"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 
 depends          "apt"
